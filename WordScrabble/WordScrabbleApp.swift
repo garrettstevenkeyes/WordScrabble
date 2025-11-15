fc1f10e4cf607ab3e57ce7f9f300c6e0fac12a9c
Garrett Keyes <garrett.keyes@gmail.com>
2025-11-15 12:03:49 -0500

    Initial Commit

---
+++ WordScrabble/WordScrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrabbleApp.swift
+//  WordScrabble
+//
+//  Created by Garrett Keyes on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
